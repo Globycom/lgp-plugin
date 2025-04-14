e7217415fd8c1b883f6482aeaade90e14bfc2b58
Valentin LGP <valentin@leguichetpro.com>
2025-04-14 11:39:02 +0200

    Add main

--- lgp-init.js
+++ lgp-init.js
@@ -6,9 +6,9 @@
         document.head.appendChild(script);
     };
 
-    loadScript("https://cdn.jsdelivr.net/gh/globycom/lgp-plugin@0.1/lgp-ai-agent.js");
-    loadScript("https://cdn.jsdelivr.net/gh/globycom/lgp-plugin@0.1/lgp-ai-popup.js");
-    loadScript("https://cdn.jsdelivr.net/gh/globycom/lgp-plugin@0.1/lgp-form-popup.js");
+    loadScript("https://cdn.jsdelivr.net/gh/globycom/lgp-plugin@main/lgp-ai-agent.js");
+    loadScript("https://cdn.jsdelivr.net/gh/globycom/lgp-plugin@main/lgp-ai-popup.js");
+    loadScript("https://cdn.jsdelivr.net/gh/globycom/lgp-plugin@main/lgp-form-popup.js");
 })();
 
 const addCSS = (href) => {
@@ -19,4 +19,4 @@ const addCSS = (href) => {
 };
 
 addCSS("https://cdn.jsdelivr.net/npm/@n8n/chat/dist/style.css");
-addCSS("https://cdn.jsdelivr.net/gh/globycom/lgp-plugin@0.1/popup-style.css");
+addCSS("https://cdn.jsdelivr.net/gh/globycom/lgp-plugin@main/popup-style.css");
