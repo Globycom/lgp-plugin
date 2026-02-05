840933df472e902b52e7ee775998ebfcfa3309f3
SahalVal <valentinsahali@gmail.com>
2026-02-05 10:32:39 +0100

    Update lgp-init.js

--- lgp-init.js
+++ lgp-init.js
@@ -14,13 +14,13 @@
 
 
 
-    loadScript("https://cdn.jsdelivr.net/gh/globycom/lgp-plugin@latest/lgp-ai-agent.js");
+    /*loadScript("https://cdn.jsdelivr.net/gh/globycom/lgp-plugin@latest/lgp-ai-agent.js");*/
 
     /*loadScript("https://cdn.jsdelivr.net/gh/globycom/lgp-plugin@latest/lgp-ai-popup.js");*/
 
-    /*loadScript("https://cdn.jsdelivr.net/gh/globycom/lgp-plugin@latest/lgp-callback-popup.js");*/
+    loadScript("https://cdn.jsdelivr.net/gh/globycom/lgp-plugin@latest/lgp-callback-popup.js");
 
-    /*loadScript("https://cdn.jsdelivr.net/gh/globycom/lgp-plugin@latest/lgp-form-popup.js");*/
+    loadScript("https://cdn.jsdelivr.net/gh/globycom/lgp-plugin@latest/lgp-form-popup.js");
 
 })();
 
@@ -43,3 +43,4 @@ addCSS("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;70
 addCSS("https://cdn.jsdelivr.net/npm/@n8n/chat/dist/style.css");
 
 addCSS("https://cdn.jsdelivr.net/gh/globycom/lgp-plugin@latest/popup-style.css");
+
